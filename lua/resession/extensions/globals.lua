88d0249900a94c43f4eb413f523bf7ca88b4359a
Jackaed <ed.andy.jackson@gmail.com>
2024-10-08 23:41:28 +0200

    Oops

--- lua/resession/extensions/globals.lua
+++ lua/resession/extensions/globals.lua
@@ -2,7 +2,7 @@ local M = {}
 local state = require('barbar.state')
 local render = require('barbar.ui.render')
 
-M.on_save() = function()
+M.on_save = function()
   local buffers = state.export_buffers()
   return "lua require('barbar.state').restore_buffers " .. vim.inspect(buffers, {newline = ' ', indent = ''})
 end
